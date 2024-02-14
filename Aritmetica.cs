e37429cbf6b63fca0cd7d5e0832600bd16efa61f
Ester Tavares <157421251+EsterTavaresOliveira@users.noreply.github.com>
2024-02-14 14:17:09 +0000

    Criar Class Aritmética e criar método

---
+++ Aritmetica.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculos
+{  // Implementa operações aritméticas 
+    internal class Aritmetica
+    {
+        public int Somar (int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
