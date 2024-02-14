aa5c48b9f4b5afebcb8e92331b16eec43329428b
Ester Tavares <157421251+EsterTavaresOliveira@users.noreply.github.com>
2024-02-14 14:24:14 +0000

    Criar método Multiplicar

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -6,12 +6,17 @@ namespace Calculos
     {
         public int Somar (int x, int y)
         {
-            return x * y;
+            return x + y;
         }
         public int Subtrair(int x, int y)
         {
             return x - y;
         }
+        public int Multiplicar (int x, int y)
+        {
+            return x * y;
+        }
+
 
     }
 }
