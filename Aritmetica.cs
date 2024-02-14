61bd56f4e92414f075c608ac6983f9f8062df91f
Ester Tavares <157421251+EsterTavaresOliveira@users.noreply.github.com>
2024-02-14 14:28:27 +0000

    Adicionar comentários

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -4,14 +4,20 @@ namespace Calculos
 {  // Implementa operações aritméticas 
     internal class Aritmetica
     {
+        //operação soma
+        //retorna soma de 2 valores
         public int Somar (int x, int y)
         {
             return x + y;
         }
+        //operação subtração
+        //retorna subtração de 2 valores
         public int Subtrair(int x, int y)
         {
             return x - y;
         }
+        //operação multiplicação
+        //retorna multiplicação de 2 valores
         public int Multiplicar (int x, int y)
         {
             return x * y;
